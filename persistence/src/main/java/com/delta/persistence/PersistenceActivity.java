8e6ee6894e646d62b668a69da7bf48e9b1dc9760
learnovate <spawrks@learnovatelabs.com>
2014-04-11 19:48:24 -0500

    .gitignore file added

---
+++ persistence/src/main/java/com/delta/persistence/PersistenceActivity.java
@@ -0,0 +1,66 @@
+package com.delta.persistence;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class PersistenceActivity extends Activity {
+
+
+    TextView readingView;
+    TextView appRestartsView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_persistence);
+
+
+    }
+
+    //Reading and Writing
+
+    public String getTextFile() {
+
+        return null;
+
+    }
+
+    public void saveTextFile(String content) {
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.persistence, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
