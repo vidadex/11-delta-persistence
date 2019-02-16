259a97ae2f8eaa176ef7b6d4db17a160d2c77346
Itang Uwem Itang <itanguwem@gmail.com>
2019-02-16 20:49:06 +0100

    All addded modifications

--- persistence/src/main/java/com/delta/persistence/PersistenceActivity.java
+++ persistence/src/main/java/com/delta/persistence/PersistenceActivity.java
@@ -22,26 +22,112 @@ public class PersistenceActivity extends Activity {
     TextView readingView;
     TextView appRestartsView;
 
+    private static final String NUMBER_OF_TIMES_RUN_KEY = "NUMBER_OF_TIMES_RUN_KEY";
+    private static final String DATA_FILE = "my_file";
+    private int howManyTimesBeenRun = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_persistence);
 
+        readingView = (TextView) findViewById(R.id.displayView);
+        appRestartsView = (TextView) findViewById(R.id.applicationRestarts);
 
+        SharedPreferences sharedPreferences = getPreferences(Context.MODE_PRIVATE);
+
+        //read
+        int defaultValue = 0;
+        howManyTimesBeenRun = sharedPreferences.getInt(NUMBER_OF_TIMES_RUN_KEY, defaultValue);
+
+        //first time message
+        if (howManyTimesBeenRun == 0) {
+            Toast.makeText(this, "Welcome To Your New Magic Notepad", Toast.LENGTH_LONG).show();
+        }
+        howManyTimesBeenRun++;
+
+        //write
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(NUMBER_OF_TIMES_RUN_KEY, howManyTimesBeenRun);
+        editor.commit();
+
+        appRestartsView.setText(String.valueOf(howManyTimesBeenRun));
+
+    }
+
+    //Persist the data when app is minimized or shut down
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveTextFile(readingView.getText().toString());
+    }
+
+    //Persist the data when app is started or resumed
+    @Override
+    protected void onResume() {
+        super.onResume();
+        readingView.setText(getTextFile());
     }
 
     //Reading and Writing
 
     public String getTextFile() {
 
-        return null;
+        FileInputStream fileInputStream = null;
+        String fileData = null;
+
+        try {
+            fileInputStream = openFileInput(DATA_FILE);
+            int size = fileInputStream.available();
+            byte[] buffer = new byte[size];
+            fileInputStream.read(buffer);
+            fileInputStream.close();
+            fileData = new String(buffer, "UTF-8");
+        } catch (FileNotFoundException e) {
+            Log.e("FILE", "Couldnt find the file");
+            e.printStackTrace();
+        } catch (IOException e) {
+            Log.e("FILE", "Error");
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fileInputStream != null) {
+                    fileInputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return fileData;
 
     }
 
     public void saveTextFile(String content) {
 
+        FileOutputStream fileOutputStream = null;
+
+        try {
+            fileOutputStream = openFileOutput(DATA_FILE, Context.MODE_PRIVATE);
+            fileOutputStream.write(content.getBytes());
+        } catch (FileNotFoundException e) {
+            Log.e("FILE", "Couldnt find that file");
+            e.printStackTrace();
+        } catch (IOException e) {
+            Log.e("File", "ID Error");
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fileOutputStream != null) {
+                    fileOutputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
